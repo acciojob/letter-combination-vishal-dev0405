34c51438fd7260dc376aa1698c33a177e448c40d
Vishal Verma <100039120+vishal-dev0405@users.noreply.github.com>
2023-01-23 16:56:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function letterCombinations(input_digit) {
-  //Complete the function
+  if (!input_digit) return [];
+  let result = [''];
+  for (let input_digit of input_digit) {
+    const letters = mapping[input_digit];
+    const temp = [];
+    for (let letter of letters) {
+      for (let combination of result) {
+        temp.push(combination + letter);
+      }
+    }
+    result = temp;
+  }
+  return result.sort();
 }
 
 module.exports = letterCombinations;
